859ad3118aee5cb176dc7a487ac4fbc3729d6f59
Avyukth <subhrajit.makur@hotmail.com>
2026-01-23 13:29:58 +0530

    refactor(green): add format/index.ts barrel export for priority utilities

---
+++ src/lib/utils/format/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Format utilities barrel export
+ */
+
+export {
+	beadPriorityToMail,
+	mailPriorityToBead,
+	formatPriorityDisplay,
+	formatPriorityShort,
+	isValidBeadPriority,
+	isValidMailPriority,
+	type BeadPriority,
+	type MailPriority
+} from './priority';
